0ec6d7b57935b524dacdc329f79c41c19255461b
Mohitha <122733485+MohithaaU@users.noreply.github.com>
2026-05-24 12:51:50 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,17 +1,15 @@
 from ultralytics import YOLO
 import cv2
 
-# Load your trained model
 model = YOLO("bestv4.pt")
-# Run detection on your image
+
 results = model("t2.jpg",conf=0.5,iou=0.3)
 
-# Show the result
 results[0].show()
 
 metrics = model.val(
     data="data.yaml",
-    split="test"  # uses test folder specifically
+    split="test"  
 )
 
 print("mAP50:", metrics.box.map50)
